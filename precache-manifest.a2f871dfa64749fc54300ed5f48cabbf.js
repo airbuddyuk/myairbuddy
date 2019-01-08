9fa8d431a68b847e412842dcecd8d5e662a28034
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2019-01-08 21:05:36 +0000

    Updates

---
+++ precache-manifest.a2f871dfa64749fc54300ed5f48cabbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myairbuddy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2ced8ad49508292644fb",
+    "url": "/myairbuddy/static/js/runtime~main.2ced8ad4.js"
+  },
+  {
+    "revision": "50adffbca7e1b2a47fb7",
+    "url": "/myairbuddy/static/js/main.50adffbc.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/myairbuddy/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "50adffbca7e1b2a47fb7",
+    "url": "/myairbuddy/static/css/main.d4c5dcf1.chunk.css"
+  },
+  {
+    "revision": "cee25a64d9e7ec79da62e73276bd2469",
+    "url": "/myairbuddy/index.html"
+  }
+];
